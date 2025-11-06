b5ed139eb80f40b39651070f629e90c2267e5266
Hasan Alibegić <hasan461@gmail.com>
2025-11-06 10:39:59 +0100

    Promise Polyfill

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// Promise polyfill for older browsers
+import "promise-polyfill/src/polyfill";
+
 import { LivePlayer, type LivePlayerProps } from "./components/live-player";
 import { VodPlayer, type VodPlayerProps } from "./components/vod-player";
 
